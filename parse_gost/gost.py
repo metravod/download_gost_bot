c02db10b30ade21e4b6756edc7ca072d8a1b405b
metravod <arttrek42@gmail.com>
2023-04-24 13:08:56 +0300

    logger

--- parse_gost/gost.py
+++ parse_gost/gost.py
@@ -11,12 +11,12 @@ from PyPDF2 import PdfReader, PdfWriter
 
 
 logger = logging.getLogger(__name__)
-# logger.setLevel(logging.DEBUG)
-# handler = logging.FileHandler('app.log')
-# handler.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# handler.setFormatter(formatter)
-# logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 class Gost:
